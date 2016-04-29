52a51b93c402ddb509438228d0eb69b1b03d2cf8
chenfeng <543259736@qq.com>
2016-04-29 23:55:17 +0800

    修改baseUrl地址

--- client/app/core/app.config.js
+++ client/app/core/app.config.js
@@ -107,8 +107,8 @@
 
   function api($http) {
 
-    //var baseUrl = 'http://101.201.198.27';
-    var baseUrl = location.protocol + '//' + location.hostname + '/api';
+    var baseUrl = 'http://101.201.198.27';
+    //var baseUrl = location.protocol + '//' + location.hostname + '/api';
     var api = {
       studio: {},
       user: {},
